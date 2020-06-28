8f0737fa358d35b5ab906ad191155447bbc65026
seyi-adeleke <seyiadeleke42@yahoo.com>
2020-06-28 19:56:05 +0100

    add getVersion method

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+const ROOT_URL = 'https://dev.codementor.io/api/';
+
+class Codementor {
+    apiKey:string;
+    constructor(apiKey: string) {
+        this.apiKey = apiKey;
+        if (typeof apiKey !== 'string') {
+            throw new Error('Api Key should be a string');
+        }
+    }
+
+    getVersion() {
+        return '0.0.1';
+    }
+}
+
+
+export = Codementor;
