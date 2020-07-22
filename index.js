ce9cf51ed3928f98c8e1dac9a8737a954e6f6d4e
seyi-adeleke <seyiadeleke42@yahoo.com>
2020-07-22 11:05:38 +0100

    update gitignore

--- index.js
+++
@@ -1,9 +0,0 @@
-const Codementor = require('./lib/index');
-
-const codementor = new Codementor('');
-
-codementor.declineSession('5eyyfic1fm').then(sessions =>{
-    console.log(sessions)
-}).catch(error =>{
-    console.log(error)
-});
